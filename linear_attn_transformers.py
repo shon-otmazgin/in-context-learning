69f2536939d3614c6a82b08b4a50df6089be84e3
shon711 <shon711@gmail.com>
2023-02-03 17:44:38 +0200

    forward == generate

--- linear_attn_transformers.py
+++ linear_attn_transformers.py
@@ -3,15 +3,35 @@ import numpy as np
 
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
-inputs = tokenizer(["Today is"], return_tensors="pt")
+inputs = tokenizer(["Nine judges currently serve the Supreme"], return_tensors="pt")
 
-# Example 1: Print the scores for each token generated with Greedy Search
-outputs = model.generate(**inputs, max_new_tokens=5, return_dict_in_generate=True, output_scores=True)
-transition_scores = model.compute_transition_scores(
-    outputs.sequences, outputs.scores, normalize_logits=True
-)
-input_length = inputs.input_ids.shape[1]
-generated_tokens = outputs.sequences[:, input_length:]
-for tok, score in zip(generated_tokens[0], transition_scores[0]):
-    # | token | token string | logits | probability
-    print(f"| {tok:5d} | {tokenizer.decode(tok):8s} | {score.numpy():.3f} | {np.exp(score.numpy()):.2%}")
+# simple forward
+outputs = model(**inputs)
+logits = outputs.logits
+input_ids = inputs["input_ids"].tolist()[0]
+ids = logits.argmax(dim=-1).tolist()[0]
+
+# simple forward
+
+texts = []
+for i, _id in enumerate(ids):
+    t, p = tokenizer.decode(input_ids[:i+1]), tokenizer.decode(_id)
+    texts.append(t)
+    print(f'prefix: {t} \nnext word: {p}')
+    print()
+
+# using generate
+
+for t in texts:
+    inputs = tokenizer([t], return_tensors="pt")
+    outputs = model.generate(**inputs, max_new_tokens=1, return_dict_in_generate=True, output_scores=True)
+    transition_scores = model.compute_transition_scores(
+        outputs.sequences, outputs.scores, normalize_logits=True
+    )
+    input_length = inputs.input_ids.shape[1]
+    generated_tokens = outputs.sequences[:, input_length:]
+    print(f'prefix: {t}')
+    for tok, score in zip(generated_tokens[0], transition_scores[0]):
+        # | token | token string | logits | probability
+        print(f"| {tok:5d} | {tokenizer.decode(tok):8s} | {score.numpy():.3f} | {np.exp(score.numpy()):.2%}")
+    print()
