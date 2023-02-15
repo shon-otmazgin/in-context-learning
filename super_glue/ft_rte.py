9ee65e41791b8330c0e7835a93b9037bdd221a8f
shon711 <shon711@gmail.com>
2023-02-15 11:23:34 +0200

    refactor FT

--- super_glue/ft_rte.py
+++ super_glue/ft_rte.py
@@ -3,58 +3,64 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments,
     DataCollatorForLanguageModeling
 
 from data import get_datasets
-from config import rte_prompt, rte_config
+from config import prompts
 from utils import Project2TargetTokens, EvaluateFirstStepCallback
 
 from peft import get_peft_model, LoraConfig
 
 
 if __name__ == '__main__':
-    cache_dir = '/home/nlp/shon711/.cache'
+    task_name = 'rte'
+    n_shots = 16
+    seed = 0
+
     opt_size = 'facebook/opt-13b'
-    experiment_name = f'rte-{opt_size}'
+    cache_dir = '/home/nlp/shon711/.cache'
+    experiment_name = f'FT-{task_name}-{opt_size}'
 
     wandb.init(project='in-context-learning', name=experiment_name)
-
-    tokenizer = AutoTokenizer.from_pretrained(opt_size, use_fast=False, cache_dir=cache_dir)
-
-    few_shot_dataset, dev_dataset = get_datasets(
-        task_name='rte', prompter=rte_prompt, n_shots=16, seed=0, finetune=True, tokenizer=tokenizer
-    )
 
     training_args = TrainingArguments(
         output_dir=experiment_name,
         logging_dir=experiment_name,
         overwrite_output_dir=True,
         learning_rate=3e-4,
-        # gradient_accumulation_steps=len(few_shot_dataset),
-        max_steps=len(few_shot_dataset),
+        gradient_accumulation_steps=2,
+        max_steps=64,
         per_device_train_batch_size=1,
         per_device_eval_batch_size=1,
-        save_strategy='no',
-        evaluation_strategy='no',
-        # eval_steps=1,
-        # logging_first_step=True,
+        evaluation_strategy='steps',
+        eval_steps=n_shots,
         logging_strategy='steps',
         logging_steps=4,
-        remove_unused_columns=True,
-        seed=0,
+        seed=seed,
         fp16=True,
-        report_to='wandb',
+        report_to=['wandb'],
         run_name=experiment_name,
+        save_strategy='steps',
+        save_steps=n_shots,
+        save_total_limit=1,
+        metric_for_best_model='accuracy',
+        greater_is_better=True,
         deepspeed='ds_zero2_config.json'
     )
 
-    data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
-    target_tokens = [x[0] for x in tokenizer(rte_config['target_tokens'], add_special_tokens=False)['input_ids']]
-    logits_processor = Project2TargetTokens(target_tokens)
-
+    prompter = prompts[task_name]
+    tokenizer = AutoTokenizer.from_pretrained(opt_size, use_fast=False, cache_dir=cache_dir)
     model = AutoModelForCausalLM.from_pretrained(opt_size, cache_dir=cache_dir)
     peft_config = LoraConfig(
         task_type="CAUSAL_LM", inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
     )
     model = get_peft_model(model, peft_config)
     print(model.print_trainable_parameters())
+
+    few_shot_dataset, dev_dataset = get_datasets(
+        task_name=task_name, prompter=prompter, n_shots=n_shots, seed=seed, tokenizer=tokenizer
+    )
+    data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    target_tokens = list(prompter.target_tokens.values())
+    encoded_target_tokens = [x[0] for x in tokenizer(target_tokens, add_special_tokens=False)['input_ids']]
+    target_tokens_logits_processor = Project2TargetTokens(encoded_target_tokens)
 
     trainer = Trainer(
         model=model,
@@ -63,8 +69,8 @@ if __name__ == '__main__':
         train_dataset=few_shot_dataset,
         eval_dataset=dev_dataset,
         data_collator=data_collator,
-        compute_metrics=logits_processor.eval,
-        preprocess_logits_for_metrics=logits_processor
+        preprocess_logits_for_metrics=target_tokens_logits_processor,
+        compute_metrics=target_tokens_logits_processor.eval,
     )
     trainer.add_callback(EvaluateFirstStepCallback())
 
